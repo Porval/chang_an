48c203da4a8314261a0030a109680d6072599f4f
jiangpeng <jiangpeng@qima-inc.com>
2017-10-06 20:44:47 +0800

    set up identity page

--- pages/identity/identity.js
+++ pages/identity/identity.js
@@ -1,4 +1,4 @@
-// pages/register/register.js
+// pages/identity/identity.js
 Page({
 
   /**
@@ -36,10 +36,18 @@ Page({
   
   },
 
-  toNext: function () {
-    wx.navigateTo({
-      url: '../identity/identity',
-    })
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
   },
 
   /**
