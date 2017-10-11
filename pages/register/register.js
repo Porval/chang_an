63dd477a788d920080fccb61b6f1ef0705ec7580
jiangpeng <jiangpeng@qima-inc.com>
2017-10-11 21:42:26 +0800

    picker get resulkt

--- pages/register/register.js
+++ pages/register/register.js
@@ -5,7 +5,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    area: ['北京市', '天津市', '河北省', '山西省', '内蒙古自治区', '辽宁省', '吉林省', '黑龙江省', '上海市', '江苏省', '浙江省', '安徽省', '福建省', '江西省', '山东省', '河南省', '湖北省', '湖南省', '广东省', '广西壮族自治区', '海南省', '重庆市', '四川省', '贵州省', '云南省', '西藏自治区', '陕西省', '甘肃省', '青海省', '宁夏回族自治区', '新疆维吾尔自治区', '台湾省', '香港特别行政区', '澳门特别行政区'],
+    areaIndex: 0,
+    mobile: null,
+    smsCode: null,
+    psw: null,
+    confirmPsw: null,
+    isCheckedAgreement: false
   },
 
   /**
@@ -15,44 +21,44 @@ Page({
   
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  phoneInput: function(e) {
+      this.setData({
+          mobile: e.detail.value
+      });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  smsCodeInput: function(e) {
+      this.setData({
+          smsCode: e.detail.value
+      });
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  pswInput: function(e) {
+      this.setData({
+          psw: e.detail.value
+      });
+  },
+
+  confirmPswInput: function(e) {
+      this.setData({
+          confirmPsw: e.detail.value
+      });
+  },
+
+  checkboxChange: function(e) {
+    console.log(e);
+      this.setData({
+          isCheckedAgreement: e.detail.value.length > 0 
+      });
+  },
+
+  onAreaChange: function(e) {
+      this.setData({
+          areaIndex: e.detail.value
+      })
   },
 
   toNext: function () {
-    wx.navigateTo({
-      url: '../identity/identity',
-    })
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+      
   }
 })
