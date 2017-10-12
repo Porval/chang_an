7be18267a5ed9cbc006954478090f3a4fa46905a
jiangpeng <jiangpeng@qima-inc.com>
2017-10-12 20:30:16 +0800

    debug for get register info

--- pages/register/register.js
+++ pages/register/register.js
@@ -6,7 +6,7 @@ function countdown(that) {
     that.setData({  
       btnText: "获取验证码",
       btnStyle: "zan-btn--primary",
-      second: 5
+      second: 60
     });  
   return ;  
  }  
@@ -30,14 +30,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-    area: ['北京市', '天津市', '河北省', '山西省', '内蒙古自治区', '辽宁省', '吉林省', '黑龙江省', '上海市', '江苏省', '浙江省', '安徽省', '福建省', '江西省', '山东省', '河南省', '湖北省', '湖南省', '广东省', '广西壮族自治区', '海南省', '重庆市', '四川省', '贵州省', '云南省', '西藏自治区', '陕西省', '甘肃省', '青海省', '宁夏回族自治区', '新疆维吾尔自治区', '台湾省', '香港特别行政区', '澳门特别行政区'],
+    area: [],
     areaIndex: 0,
+    areaCode: [],
     mobile: null,
     smsCode: null,
     psw: null,
     confirmPsw: null,
     isCheckedAgreement: false,
-    second: 5,
+    second: 60,
     btnText: "获取验证码",
     btnStyle: "zan-btn--primary"
   },
@@ -47,6 +48,34 @@ Page({
    */
   onLoad: function (options) {
     this.service = service(this);
+
+    this.service({
+        api: '/app/official/getAllHost.ashx',
+        success: (res) => {
+            if(res.list) {
+                var areaList = [];
+                var areaCodeList = [];
+                for(var index in res.list) {
+                    areaList.push(res.list[index].name);
+                    areaCodeList.push(res.list[index].host);
+                }
+                this.setData({
+                    area: areaList,
+                    areaCode: areaCodeList
+                })
+            }
+            console.log("get area list " + res);
+        },
+        fail: (res)=> {
+            wx.showToast({
+                title: "获取信息失败"
+            })
+
+            wx.reLaunch({
+              url: "./index/index"
+            })
+        } 
+    });
   },
 
   phoneInput: function(e) {
@@ -125,6 +154,24 @@ Page({
   },
 
   toNext: function () {
-      
+    this.service({
+        api: '/app/official/register.ashx',
+        query: {
+          hostId: this.data.areaCode[this.data.areaIndex],
+          mobile: this.data.mobile,
+          validCode: this.data.smsCode,
+          txtPassWord: this.data.psw,
+          txtRePassWord: this.data.confirmPsw,
+          from: 6
+        },
+        success: (res) => {
+            console.log("register success " + res);
+        },
+        fail: (res)=> {
+            wx.showToast({
+                title: "发送失败请重试"
+            })
+        } 
+    });
   }
 })
